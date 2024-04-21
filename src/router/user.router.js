34110f9a1ddc474a030c01a4f80545d79d4ca90d
judit Bardon <juditbardonromero@gmail.com>
2024-04-21 20:35:29 +0100

    resolver conflictos

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -2,12 +2,8 @@ const {Router} = require ('express')
 const router = Router();
 const xperggCtrl = require ('../controller/user.controller');
 
-<<<<<<< HEAD
-router.post('/user/:id', xperggCtrl.register);
-=======
 router.post('/user/:id', xperggCtrl.addUserApi);
 router.post('/login', xperggCtrl.login);
->>>>>>> f7378931535e9e3cd6fb01538229e764bc4e77be
 router.get('/user/:id/friends', xperggCtrl.getUserAndFriendsById);
 router.get('/user/:id/interests', xperggCtrl.getUserInterests);
 router.put('/users/:id/available', xperggCtrl.updateUserAvailableApi);
