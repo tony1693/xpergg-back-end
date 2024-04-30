5c2f33e919b7ccbf60780f2a51d3c3c7b94e4e82
judit Bardon <juditbardonromero@gmail.com>
2024-04-30 18:01:40 +0100

    ordenar threads

--- src/router/threads.router.js
+++ src/router/threads.router.js
@@ -5,7 +5,6 @@ const xperggCtrl = require ('../controller/threads.controller');
 
 router.post('/threads_messages/:id', xperggCtrl.insertMessageThread);
 router.get('/threads', xperggCtrl.getThreads);
-router.get('/thread', xperggCtrl.getOneThread)
 router.post('/threads', xperggCtrl.postThread);
 router.get('/threads-messages-users', xperggCtrl.getThreadsMessagesUsers);
 
