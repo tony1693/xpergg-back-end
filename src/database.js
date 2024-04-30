0762692f942c27d97a19300b14f16ca40b59021a
MiguelMO2311 <mmeneses73@gmail.com>
2024-04-30 23:58:19 +0200

    update user edit profile

--- src/database.js
+++ src/database.js
@@ -4,11 +4,7 @@ async function createConnection() {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-<<<<<<< HEAD
         password: 'Meneses23', // meter aquí vuestra contraseña de acceso a la BDD.
-=======
-        password: 'codenotch1805', // meter aquí vuestra contraseña de acceso a la BDD.
->>>>>>> 9cf550e6cd97072b741d05dfc8df776ab1751da1
         database: 'xpergg',
         waitForConnections: true,
         connectionLimit: 10,
