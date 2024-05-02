1b6c049d121699eb64f53dcb7a843e095c279afa
MiguelMO2311 <mmeneses73@gmail.com>
2024-05-02 19:45:47 +0200

    profile

--- src/router/publications.router.js
+++ src/router/publications.router.js
@@ -3,10 +3,8 @@ const router = Router();
 const xperggCtrl = require ('../controller/publications.controller');
 
 router.post('/comments', xperggCtrl.addComment);
-router.post('/postcommentuser', xperggCtrl.addPostCommentUser);
 router.get('/posts', xperggCtrl.getPostsApi);
 router.post('/posts', xperggCtrl.addPostApi);
-router.get('/postsUser',xperggCtrl.getPostsFromUser),
 router.get('/getUserPostCount', xperggCtrl.getUserPostCount);
 
 
